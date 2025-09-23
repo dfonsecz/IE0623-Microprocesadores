690a4ac42fb22d97a1ef7de99ab69d127c01727e
dfonsecz <daniela.fonsecazumbado@gmail.com>
2025-09-23 09:23:26 -0700

    Arreglar problema en el algoritmo BCD-BIN

--- 02-Tareas/02-Tarea-2/DANIELAFONSECA_P3_T2.asm
+++ 02-Tareas/02-Tarea-2/DANIELAFONSECA_P3_T2.asm
@@ -19,8 +19,8 @@
 ;*******************************************************************************
 
         ORG $1000
-Binario:   dw $0FA5
-BCD:       dw $5555
+Binario:   dw $0234
+BCD:       dw $9999
 
         ORG $1010
 Num_BCD:   ds 2
@@ -41,67 +41,63 @@ Temp:      ds 2
 ;*******************************************************************************
 
                 ORG $2000
-Conversiones    Ldd Binario
-                Jsr BIN-BCD
+                Lds #$3FFF
+                Ldd Binario
+                Jsr BIN_BCD
                 Ldd BCD
-                Jsr BCD-BIN
+                Jsr BCD_BIN
                 Bra *
 
-BIN-BCD         Clr BCD_L
+BIN_BCD         Clr BCD_L
                 Clr BCD_H
                 Clr Num_BCD
                 Ldx #4
-ShiftL-4b       Cpx #0                  ; Despues de hacer shift izq por 4 bits,
+ShiftL_4b       Cpx #0                  ; Despues de hacer shift izq por 4 bits,
                 Beq Algoritmo           ; continuar a Algoritmo
                 Lsld
-                Std Temp
-                Tfr X,A
-                Suba #1
-                Tfr A,X
-                Ldd Temp
-                Bra ShiftL-4b           ; Regresar al inicio del ciclo
+                Dex
+                Bra ShiftL_4b           ; Regresar al inicio del ciclo
 Algoritmo       Ldx #12                 ; Se debe hacer shift por 12 bits
-Ciclo-BIN-BCD   Lsld
+Ciclo_BIN_BCD   Lsld
                 Rol BCD_L
                 Rol BCD_H
                 Std Temp
-Eval-Nib1       Ldaa BCD_L              ; Evaluar el nibble menos significa-
+Eval_Nib1       Ldaa BCD_L              ; Evaluar el nibble menos significa-
                 Anda #$0F               ; tivo de variable con la parte baja
                 Cmpa #$05               ; Si (A)>=5, sumar 3 al nibble, sino
-                Beq Eval-Nib2           ; continuar con la evaluacion del si-
+                Bcs Eval_Nib2           ; continuar con la evaluacion del si-
                 Adda #$03               ; guiente nibble
-Eval-Nib2       Ldab BCD_L              ; Evaluar el nibble mas significativo
+Eval_Nib2       Ldab BCD_L              ; Evaluar el nibble mas significativo
                 Andb #$F0               ; de la variable con la parte baja
                 Cmpb #$50
-                Beq Sum-Nib-1-2
+                Bcs Sum_Nib_1_2
                 Addb #$30
-Sum-Nib-1-2     Aba
+Sum_Nib_1_2     Aba                     ; Sumar msb con lsb
                 Staa BCD_L              ; Reemplazar variable con la parte baja
-Eval-Nib3       Ldaa BCD_H              ; Evaluar el nibble menos significa-
+Eval_Nib3       Ldaa BCD_H              ; Evaluar el nibble menos significa-
                 Anda #$0F               ; tivo de la variable con la parte alta
                 Cmpa #$05
-                Beq Eval-Nib4
+                Bcs Eval_Nib4
                 Adda #$03
-Eval-Nib4       Ldab BCD_H              ; Evaluar el nibble mas significativo
+Eval_Nib4       Ldab BCD_H              ; Evaluar el nibble mas significativo
                 Andb #$F0               ; de la variable con la parte alta
                 Cmpb #$50
-                Beq Sum-Nib-3-4
+                Bcs Sum_Nib_3_4
                 Addb #30
-Sum-Nib-3-4     Aba
+Sum_Nib_3_4     Aba                     ; Sumar msb con lsb
                 Staa BCD_H              ; Reemplazar variable con la parte alta
-                Tfr X,A
-                Suba #1
-                Cmpa #0
-                Beq Fin-BIN-BCD
-                Tfr A,X
+                Dex
+		Tfr X,A
+                Tsta
+                Beq Fin_BIN_BCD
                 Ldd Temp
-                Bra Ciclo-BIN-BCD       ; Regresar al inicio del ciclo
-Fin-BIN-BCD     Ldaa BCD_H
+                Bra Ciclo_BIN_BCD       ; Regresar al inicio del ciclo
+Fin_BIN_BCD     Ldaa BCD_H
                 Ldab BCD_L
                 Std Num_BCD             ; Guardar resultado en espacio asignado
                 Rts
 
-BCD-BIN         Staa BCD_H
+BCD_BIN         Staa BCD_H
                 Stab BCD_L
                 Clr Num_BIN
                 Clra
@@ -111,10 +107,10 @@ BCD-BIN         Staa BCD_H
                 Ldab BCD_L
                 Andb #$F0               ; Evaluar los 4 msb de la parte baja
                 Ldaa #4
-ShiftR-C-4b     Lsrb
-                Suba #1
-                Cmpa #0
-                Bne ShiftR-C-4b
+ShiftR_C_4b     Lsrb
+                Deca
+                Tsta
+                Bne ShiftR_C_4b
                 Ldy #10
                 Emul
                 Addd Num_BIN
@@ -126,13 +122,13 @@ ShiftR-C-4b     Lsrb
                 Emul
                 Addd Num_BIN
                 Std Num_BIN             ; Guardar el resultado de centenas
-                Stab BCD_H
+                Ldab BCD_H
                 Andb #$F0               ; Evaluar los 4 msb de la parte alta
                 Ldaa #4
-ShiftR-M-4b     Lsrb
-                Suba
+ShiftR_M_4b     Lsrb
+                Deca
                 Cmpa #0
-                Bne ShiftR-M-4b
+                Bne ShiftR_M_4b
                 Ldy #1000
                 Emul
                 Addd Num_BIN
